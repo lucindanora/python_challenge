14f05087106f681bc6345d47ac9042d66289cace
lucindanora <144958544+lucindanora@users.noreply.github.com>
2023-10-21 16:41:09 -0500

    Final changes to Python Challenge code

---
+++ PyBank/mainbank.py
@@ -0,0 +1,69 @@
+#Import modules
+import csv
+import os
+
+#Setting file path to csv file
+budget_path= os.path.join("Resources/budget_data.csv")
+
+#Open/Read csv 
+
+with open(budget_path) as budget_data:
+    budget_reader = csv.reader(budget_data, delimiter=",")
+
+    #skip header
+    budget_header = next(budget_reader)
+
+    #list for profits and months
+    profits=[]
+    months=[]
+
+    i=0
+    revenue_total=0
+
+    #list for revenue changes and initialize variable for previous row data
+    revenue_changes=[]
+    previous_row=[]
+
+    #iterate through csv rows, add change to list and update
+
+    for row in budget_reader:
+        revenue_change=0
+        
+        if i !=0:
+            revenue_change=int(row[1])-int(previous_row[1])
+
+            revenue_changes.append(revenue_change)
+
+        profits.append(int(row[1]))
+        months.append(row[0])
+
+        previous_row=row
+
+        i =+ 1
+    #actual calculations
+    revenue_avg=sum(revenue_changes)/len(revenue_changes)
+    total_months=len(months)
+    greatest_inc=max(revenue_changes)
+    greatest_dec=min(revenue_changes)
+
+    #print results to console
+    print("Analysis")
+    print("------------------------")
+    print(f'Total Months:{total_months}')
+    print(f'Total: ${sum(profits)}')
+    print(f'Average Change: ${revenue_avg:.2f}')
+    print(f'Greatest Incease in Profits: ${greatest_inc}')
+    print(f'Greatest Decrease in Profits: ${greatest_dec}')
+
+    #write results to text file in separate analysis file
+    file=open("Analysis/analysis.txt", "w")
+
+    file.write("Analysis\n")
+    file.write("----------------------------\n")
+    file.write(f'Total Months:{total_months}\n')
+    file.write(f'Total: ${sum(profits)}\n')
+    file.write(f'Average Change: ${revenue_avg:.2f}\n')
+    file.write(f'Greatest Incease in Profits: ${greatest_inc}\n')
+    file.write(f'Greatest Decrease in Profits: ${greatest_dec}\n')
+
+
---
+++ PyPoll/mainp.py
@@ -0,0 +1,84 @@
+#import modules
+import os
+import csv
+
+#setting file path
+
+election_data = "Resources/election_data.csv"
+
+#converts csv file to dictionary
+
+
+def processPollData(file):
+    #in class dictionary activities (actors, hobby_book, comicbooks.py)
+    #used class functions example and https://www.w3schools.com/python/python_functions.asp 
+    voterdict = {}
+    with open(file,"r") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter = ",")
+        csv_header = next(csvreader)
+
+        for row in csvreader: 
+            #checks if candidate has been seen yet in the csv, and adds votes as appropriate
+           
+            if row[2] in voterdict.keys():
+                voterdict[row[2]] += 1
+            else:
+                voterdict[row[2]] = 1
+    return voterdict
+
+
+#Find winning candidate by using max(), export .txt and placing .txt file in analysis folder
+# To write.txt file used in class main_solution.py, and https://www.pythontutorial.net/python-basics/python-write-text-file/ 
+# and https://www.w3schools.com/python/python_file_write.asp
+
+def writeReport(cdict):
+    with open("Analysis/voteresults.txt","w") as textfile:
+        textfile.write("Election Results\n")
+        textfile.write("____________________\n")
+        votes = cdict.values()
+        total = sum(votes)
+        maxvotes = max(votes)
+        winnersList = []
+        textfile.write(f"Total Votes: {total}\n")
+        textfile.write("________________________________________\n")
+        for name, votes in cdict.items():
+            textfile.write(f"{name}  {votes / total * 100:.3f}% ({votes})\n")
+            if votes == maxvotes:
+                winnersList.append(name)
+        textfile.write("______________________\n")
+        textfile.write("Winner: ")
+        for w in winnersList:
+            textfile.write(w + " ")
+        textfile.write("\n_______________________")
+
+#prints results to terminal
+def reporting(cdict):
+    print("Election Results")
+    print("___________________")
+    votes = cdict.values()
+    total = sum(votes)
+    maxvotes =  max(votes)
+    winnersList = []
+    print(f"Total Votes: {total}")
+    print("______________________________________")
+    for name,votes in cdict.items():
+        print(f"{name}  {votes/total * 100:,.3f}% ({votes})")
+        if votes == maxvotes:
+            winnersList.append(name)
+    print("______________________")
+    print("Winner: ",end=" ")
+    for w in winnersList:
+        print(w + " ",end =' ')
+    print()
+    print("______________________")
+
+
+
+# runs functions seen above
+def main():
+    candidatedict = {}
+    candidatedict = processPollData(election_data)
+    reporting(candidatedict)
+    writeReport(candidatedict)
+
+main()
